f2afde7f93ffb9e7e5d7169f3fd2254b070556cb
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2022-06-13 15:54:55 +0200

    [ADDS] camera speed modifier [EDITS] time for click

--- RavelTemplate/Assets/Scripts/Controllers/CameraController.cs
+++ RavelTemplate/Assets/Scripts/Controllers/CameraController.cs
@@ -3,10 +3,10 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    private InputAction _move, _rotateCamera,_drag;
+    private InputAction _move, _rotateCamera,_drag, _modifyCameraSpeed;
     private Vector3 _direction, _rotation;
     private InputManager _inputManager;
-    private float speed = 10f, rotateSpeed =.1f;
+    private float _speed = 10f,_fastSpeed = 50f, _rotateSpeed =.1f;
     private bool _moving;
     private bool _isDragging;
     private void Awake()
@@ -15,6 +15,9 @@ public class CameraController : MonoBehaviour
         _move =_inputManager.GetInputAction("Move");
         _rotateCamera = _inputManager.GetInputAction("RotateCamera");
         _drag = _inputManager.GetInputAction("Drag");
+        _modifyCameraSpeed = _inputManager.GetInputAction("ModifyCameraSpeed");
+        _modifyCameraSpeed.started += context => _speed = 50f;
+        _modifyCameraSpeed.canceled += context => _speed = 10f;
         _drag.started += _ => _isDragging = true; 
         _drag.canceled += _ => _isDragging = false; 
         _rotateCamera.started += ReadCameraRotate;
@@ -28,7 +31,7 @@ public class CameraController : MonoBehaviour
         if (obj.ReadValue<Vector2>().magnitude > .01 && _isDragging)
         {
             _rotation = transform.rotation.eulerAngles;
-            var newrotEulers=new Vector3(_rotation.x +-obj.ReadValue<Vector2>().y * rotateSpeed , _rotation.y+ obj.ReadValue<Vector2>().x * rotateSpeed, 0);
+            var newrotEulers=new Vector3(_rotation.x +-obj.ReadValue<Vector2>().y * _rotateSpeed , _rotation.y+ obj.ReadValue<Vector2>().x * _rotateSpeed, 0);
             if (newrotEulers.x < 90 || newrotEulers.x > 270)
             {
                 transform.rotation = Quaternion.Euler(newrotEulers);
@@ -46,22 +49,23 @@ public class CameraController : MonoBehaviour
     {
         if (_moving)
         {
-            transform.position += Move(transform, _direction) * (speed * Time.deltaTime);
+            transform.position += Move(transform, _direction) * (_speed * Time.deltaTime);
         }
     }
 
     private Vector3 Move(Transform origin,Vector3 dir)
     {
+        Vector3 temp = Vector3.zero;
+        
         if (dir.x > 0)
-            return origin.right;
+            temp += origin.right;
         if (dir.x < 0)
-            return origin.right * -1;
+            temp += origin.right * -1;
         if (dir.z > 0)
-            return origin.forward;
+            temp +=  origin.forward;
         if (dir.z < 0)
-            return origin.forward * -1;
-
-        return Vector3.zero;
+            temp += origin.forward * -1;
+        return temp.normalized;
     }
     
 }
--- RavelTemplate/Assets/Scripts/Controllers/PlayerController.cs
+++ RavelTemplate/Assets/Scripts/Controllers/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
@@ -7,22 +8,42 @@ public class PlayerController : MonoBehaviour
     private NavMeshAgent _agent;
     private InputAction _click, _mousePosition;
     private Camera _camera;
-
+    private IEnumerator _clickTimer;
+    private bool _isClick;
+        
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
         _click = InputManager.Instance.GetInputAction("Click");
         _mousePosition = InputManager.Instance.GetInputAction("MousePosition");
         _camera = Camera.main;
-        _click.started += GoToPosition;
+        _click.started += StartClickTimer;
+        _click.canceled += GoToPosition;
         _agent.enabled = false;
         transform.position = FindObjectOfType<SpawnPoint>().transform.position;
         _agent.enabled = true;
     }
 
+
+    private void StartClickTimer(InputAction.CallbackContext obj)
+    {
+        if (_clickTimer != null)
+            StopCoroutine(_clickTimer);
+        _clickTimer = ClickTimerRoutine(.3f);
+        StartCoroutine(_clickTimer);
+    }
+
+    private IEnumerator ClickTimerRoutine(float time)
+    {
+        _isClick = true;
+        yield return new WaitForSeconds(time);
+        _isClick = false;
+    }
+
     private void GoToPosition(InputAction.CallbackContext obj)
     {
-        
+        if (!_isClick)
+            return;
         RaycastHit hit;
         Ray ray = _camera.ScreenPointToRay(new Vector3(_mousePosition.ReadValue<Vector2>().x, _mousePosition.ReadValue<Vector2>().y,0 ));
         if (Physics.Raycast(ray, out hit))
