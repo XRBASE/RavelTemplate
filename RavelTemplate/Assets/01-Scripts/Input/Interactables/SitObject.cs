3c874c3f83f39195582795a1dca5380c386c9385
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2022-09-14 13:32:38 +0200

    [ADDS] chair prefab with tooltip

--- RavelTemplate/Assets/01-Scripts/Input/Interactables/SitObject.cs
+++ RavelTemplate/Assets/01-Scripts/Input/Interactables/SitObject.cs
@@ -1,11 +1,12 @@
-using System;
 using UnityEngine;
 
 public class SitObject : MonoBehaviour,IIdProvider
 {
+    [Header("Make sure that this object is not farther away than 2\nunits from the generated navmesh otherwise sitting will not work. \n\nSitting offset from the navmesh is .6 y units (see child sit transform)  ")]
     [SerializeField]
     public int chairId;
-    public Transform[] position;
+    [Tooltip("You can adjust the position and rotation of the seat with this transform")]
+    public Transform sitTransform;
     public void SetId(int id)
     {
         chairId = id;
