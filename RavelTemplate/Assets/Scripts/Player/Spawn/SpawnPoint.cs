9a3f4d44fcca4780d88816db164aaed95935fd31
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2022-10-13 12:06:53 +0200

    [EDITS] spawnpoint

--- RavelTemplate/Assets/Scripts/Player/Spawn/SpawnPoint.cs
+++ RavelTemplate/Assets/Scripts/Player/Spawn/SpawnPoint.cs
@@ -1,8 +1,19 @@
-using UnityEditor;
+using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class SpawnPoint : MonoBehaviour
 {
+    
+    public const int DEFAULT = 0;
+
+    private static Dictionary<int, SpawnPoint> _spawnPoints;
+
+    [SerializeField] private int _id;
+    
+    
     public void MoveToSpawnPoint()
     {
 
@@ -21,6 +32,10 @@ public class SpawnPoint : MonoBehaviour
         {
             base.OnInspectorGUI();
             var node = (SpawnPoint) target;
+            if(node._id== DEFAULT)
+                EditorGUILayout.HelpBox("This is the default spawnpoint", MessageType.Info);
+            else
+                EditorGUILayout.HelpBox($"Make sure you have 1 spawnpoint set as the default with id = {DEFAULT}. This will be the main spawnpoint for the players. Also make sure that each spawnpoint has a unique ID", MessageType.Info);
             if (GUILayout.Button("Snap nearest collider below"))
             {
                 if (Physics.Raycast(node.transform.position, Vector3.down, out var hit))
