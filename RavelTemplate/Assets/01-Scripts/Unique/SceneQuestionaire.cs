eab2e032e6d650541fe7c2aa77546ecb2999b64c
Lotte <Lotte@thenewbase.co>
2022-11-07 14:12:24 +0100

    [EDITS] scene edits.

--- RavelTemplate/Assets/01-Scripts/Unique/SceneQuestionaire.cs
+++ RavelTemplate/Assets/01-Scripts/Unique/SceneQuestionaire.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 
+/// <summary>
+/// Scene container for questionnaires if the photon flow is not to a users liking.
+/// Also handy for testing the questionnaire in the preview flow.
+/// </summary>
 public class SceneQuestionaire : MonoBehaviour
 {
     public Questionnaire questionnaire;
