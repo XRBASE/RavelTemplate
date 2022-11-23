b166107695682dd0834a9e70a65cbe625da1841f
DESKTOP-OU76AT9\Glenn <glenn-hoff@hotmail.com>
2022-11-23 16:19:39 +0100

    [ADDS] post processing to the scene.

--- RavelTemplate/Assets/01-Scripts/SceneTools/TweenShaderFloat.cs
+++ RavelTemplate/Assets/01-Scripts/SceneTools/TweenShaderFloat.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class TweenShaderFloat : MonoBehaviour
 {
     public string shaderPropertyName;
     public MeshRenderer meshRenderer;
-    public float startValue, endValue;
+    public float endValue;
     public float tweenTime;
+    public UnityEvent onTweenComplete;
+
 
     /// <summary>
     /// Tweens a shader property from a material instance from startValue to endValue in tweenTime
