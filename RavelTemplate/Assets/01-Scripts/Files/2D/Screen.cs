bc31eea5059209f905c85700a93e64d927cb85db
Lotte <Lotte@thenewbase.co>
2022-11-28 13:30:20 +0100

    [EDITS] screen editor script for template.

--- RavelTemplate/Assets/01-Scripts/Files/2D/Screen.cs
+++ RavelTemplate/Assets/01-Scripts/Files/2D/Screen.cs
@@ -1,4 +1,7 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Base.Ravel.Files
 {
@@ -6,5 +9,35 @@ namespace Base.Ravel.Files
     {
         [Tooltip("Canvas object for screen space, canvas will not be scaled, but content will be placed within the canvas bounds (PDF scales to width of canvas).")]
         [SerializeField] private Canvas _canvas;
+        
+#if UNITY_EDITOR
+        [CustomEditor(typeof(Screen))]
+        private class ScreenEditor : Editor
+        {
+            public override void OnInspectorGUI()
+            {
+                DrawDefaultInspector();
+                Screen s = (Screen) target;
+                if (s._id <= 0) {
+                    EditorGUILayout.HelpBox("Persistent screens should have a unique id of 1 or higher!",
+                                            MessageType.Error);
+                }
+                if (s._canvas == null || s._collider == null) {
+                    string msg = "Missing reference in screen:";
+                    if (s._canvas == null) {
+                        msg += "canvas ";
+                    }
+                    if (s._collider == null) {
+                        msg += "collider ";
+                    }
+                    EditorGUILayout.HelpBox(msg, MessageType.Error);
+                }
+                if (s._canvas != null && (((RectTransform) s._canvas.transform).pivot - new Vector2(0.5f, 0f)).magnitude > 0.001f) {
+                    EditorGUILayout.HelpBox("Canvas pivot should be in the lower center (0.5f, 0f) of the screen",
+                                            MessageType.Error);
+                }
+            }
+        }
+#endif
     }
 }
